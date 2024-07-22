abc2f41dfba0929b7bedde259629cbbe36fd8ed8
this-isdon <donald.onyino@globallinkplus.com>
2024-07-22 21:25:11 +0300

    chore: updated exports

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import en from './translations/en.json'
-import swa from './translations/swa.json'
+import swa from './translations/swahili.json'
 import { TranslationNamespaces } from './translations/types'
 
 i18n.use(initReactI18next).init({
@@ -11,13 +11,13 @@ i18n.use(initReactI18next).init({
         en: {
             translations: en
         },
-        swa: {
+        sw: {
             translations: swa
         }
-    }
+    } as const
 })
 
-i18n.languages = ['en', 'swa']
+i18n.languages = ['en', 'sw']
 
 export const t = (t: TranslationNamespaces) => i18n.t(t)
 
